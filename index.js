b1cbccae5cf43c679abac8f9f2f687ef17c0f463
Roukis <76912917+Roukis@users.noreply.github.com>
2021-01-09 20:57:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage1 = "dice"+randomNumber1+".png";
 
-var randomImageSource1 = "images/"+randomDiceImage1;
+var randomImageSource1 = randomDiceImage1;
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource1);
@@ -13,7 +13,7 @@ image1.setAttribute("src", randomImageSource1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage2 = "dice"+randomNumber2+".png";
+var randomDiceImage2 = randomNumber2+".png";
 
 var randomImageSource2 = "images/"+randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
